56cee627d6749647a0fc4a6ab53ba963c9e0c31d
GabeHenrique <angelina.barboza@hotmail.com.br>
2022-08-17 22:22:14 -0300

    -

--- Aula2/Main.cs
+++ Aula2/Main.cs
@@ -17,7 +17,7 @@ namespace AulaOO
                 Console.WriteLine("2 Lancar Nota");
                 Console.WriteLine("3 Consultar Nota");
                 Console.WriteLine("4 Listar Nota");
-                Console.WriteLine("sair (Para encerrrar)");
+                Console.WriteLine("sair (Para encerrar)");
 
  
 
@@ -32,23 +32,39 @@ namespace AulaOO
                         break;
                     case "2":
                         Console.WriteLine("Entre com Nome ou Matricula do Aluno para lancar nota:");
-                        aluno= disciplina.LocalizarAluno(Console.ReadLine());
-                        Console.WriteLine($"Localizado : Aluno: {aluno.Nome} Matricula: {aluno.Matricula}");
-                        
-                        Console.WriteLine("Entre com nota da np1");
-                        double np1 = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Entre com nota da np2");
-                        double np2 = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Entre com nota do trabalho");
-                        double trapalho = Convert.ToDouble(Console.ReadLine());
-                        
-                        disciplina.LancarNota(aluno, np1, np2, trapalho);
-                        Console.WriteLine("Nota Final: " + disciplina.MapaDeNotas[aluno]);
+                        try
+                        {
+                            aluno = disciplina.LocalizarAluno(Console.ReadLine());
+                            Console.WriteLine($"Localizado : Aluno: {aluno.Nome} Matricula: {aluno.Matricula}");
+
+                            Console.WriteLine("Entre com nota da np1");
+                            double np1 = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Entre com nota da np2");
+                            double np2 = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Entre com nota do trabalho");
+                            double trapalho = Convert.ToDouble(Console.ReadLine());
+
+                            disciplina.LancarNota(aluno, np1, np2, trapalho);
+                            Console.WriteLine("Nota Final: " + disciplina.MapaDeNotas[aluno]);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Aluno nao encontrado");
+                        }
                         break;
                     case "3":
-                        Console.WriteLine("Entre com Nome ou Matricula do Aluno para Consulta nota:");
-                        aluno = disciplina.LocalizarAluno(Console.ReadLine());
-                        Console.WriteLine($" Aluno: {aluno.Nome} Matricula: {aluno.Matricula} Nota Final:  {disciplina.MapaDeNotas[aluno]}");
+                        try
+                        {
+                            Console.WriteLine("Entre com Nome ou Matricula do Aluno para Consulta nota:");
+                            aluno = disciplina.LocalizarAluno(Console.ReadLine());
+                            Console.WriteLine(
+                                $" Aluno: {aluno.Nome} Matricula: {aluno.Matricula} Nota Final:  {disciplina.MapaDeNotas[aluno]}");
+
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Aluno nao encontrado");
+                        }
                         break;
                     case "4":
                         foreach (KeyValuePair<Aluno, double> alunoAtual in disciplina.MapaDeNotas)
